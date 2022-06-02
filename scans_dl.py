2aabfad33d704e3dab58b90d76460938d54b159f
Louis Dous Hervé <louisdousherve@gmail.com>
2022-06-02 13:09:04 +0200

    Added support for creating CBR archives

--- scans_dl.py
+++ scans_dl.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+from zipfile import ZipFile
     
 def fetch_data(url: str):
     r = requests.get(url)
@@ -11,12 +12,13 @@ def dl_img(url: str, file_path: str):
     with open(file_path, "wb") as img:
         img.write(r.content)
         
-def download(url: str, parent_folder: str="scans"):
+def download(url: str, parent_folder: str = "scans/raw"):
     """Download all the images found in the webpage at the given URL."""
     manga = url.split('/')[-1]
     output_folder = f"{parent_folder}/{manga}"
 
-    os.makedirs(output_folder)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     print("Downloading scans:")
 
@@ -30,3 +32,21 @@ def download(url: str, parent_folder: str="scans"):
         dl_img(scan_url, f"{output_folder}/{i}.{ext}")
         
     print(f"\nDownloaded {i} scans from {url} in {output_folder}.\n")
+    
+def create_cbr(folder: str, name: str = "", output_folder: str = "scans/cbr"):
+    """Create a CBR archive from all the scans contained in the specified folder."""
+    if not os.path.isdir(folder):
+        return
+        
+    if not name:
+        name = folder.split('/')[-1]
+        
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    cbr_path = f"{output_folder}/{name}.cbr"
+    with ZipFile(cbr_path, mode='w') as cbr:
+        for scan in os.listdir(folder):
+            cbr.write(f"{folder}/{scan}")
+            
+    return cbr_path
