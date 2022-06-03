d465b2de32064e02a14e84e16e29c68eb575e4bf
Louis Dous Hervé <louisdousherve@gmail.com>
2022-06-04 01:18:48 +0200

    Written a CLI to use the program

--- README.md
+++ README.md
@@ -1,27 +1,52 @@
 # scans_dl
-A (very basic) tool written in Python to download all the manga scans in a webpage.
+A basic tool written in Python to download all the manga scans in a webpage.
 
 For now, the code is pretty stupid and just looks for every HTML `<img>` tag in the webpage at the given URL, and then downloads all those images.
 
 First, install the dependencies (make sure to create and activate a virtual environment beforehand if you don't want to clutter your main Python install) :
 
-```console
+```shell
 python3 -m pip install -r requirements.txt
 ```
 
-To use the "library", one can write something like that :
+To use the program, one can write something like that :
 
-```python3
-import scans_dl
-
-scans_dl.download("https://site.com/url/of/the/manga")
+```shell
+python3 scans_dl.py download https://site.com/chapter-1
 ```
 
-By default, it will download the scans in a new directory named `scans`.  
+This command downloads all the scans of the chapter found in the webpage. It doesn't work if the website you provide has a reader where you have to turn the pages yourself.
 
-You can override that behavior by passing an additional argument to the `download` function :
-```python3
-scans_dl.download("https://site.com/url/of/the/manga", "my-scans-folder")
+You can also download multiple chapters at a time, by replacing the number of the chapter that changes in the address with '{}' and by specifying a range. For instance:
+
+```shell
+python3 scans_dl.py download https://site.com/chapter-{} --range 1-40
 ```
 
-Of course, use this tool only if you already own a physical copy of the manga! 🤓
+downloads the chapters 1 to 40.
+
+By default, it will download the scans in a new directory named `scans/raw`.  
+
+You can override that behavior by passing an additional argument:
+
+```console
+python3 scans_dl.py download https://site.com/chapter-1 --output [folder]
+```
+
+
+To archive a chapter in the CBR format, you can use that command:
+
+```console
+python3 scans_dl.py cbr path/to/folder/containing/scans
+```
+
+By default, it will save the archive in a new directory named `scans/cbr`.  
+
+For the complete documentation, use:
+
+```console
+python3 scans_dl.py --help
+```
+along with the command which you are interested in, for instance `download` or `cbr`.
+
+Of course, use this tool only if you already own a physical copy of the chapter you're downloading! 🤓
--- scans_dl.py
+++ scans_dl.py
@@ -1,4 +1,5 @@
-import os
+import sys, os
+import argparse
 import requests
 from bs4 import BeautifulSoup
 from zipfile import ZipFile
@@ -12,41 +13,135 @@ def dl_img(url: str, file_path: str):
     with open(file_path, "wb") as img:
         img.write(r.content)
         
-def download(url: str, parent_folder: str = "scans/raw"):
+def download(url: str, name: str = "", output_folder: str = "scans/raw"):
     """Download all the images found in the webpage at the given URL."""
-    manga = url.split('/')[-1]
-    output_folder = f"{parent_folder}/{manga}"
+    url = url[:-1] if url[-1] == '/' else url
+    name = url.split('/')[-1] if (not name or len(name) == 0) else name
+    output_folder = f"{output_folder}/{name}"
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    print("Downloading scans:")
+    print("Downloading scans:\n")
 
     soup = BeautifulSoup(fetch_data(url), 'html.parser')
     i = 0
     for item in soup.find_all('img'):
-        i += 1
         scan_url = item['src']
+        if scan_url[:4] != 'http':
+            continue
+        i += 1
         ext = scan_url.split('.')[-1]
-        print(f"    - {item['alt']} ({scan_url})")
-        dl_img(scan_url, f"{output_folder}/{i}.{ext}")
+        ext = '.' + ext if '/' not in ext else ".jpg"
+        print(f"    - {scan_url}")
+        dl_img(scan_url, f"{output_folder}/{i}{ext}")
         
     print(f"\nDownloaded {i} scans from {url} in {output_folder}.\n")
     
-def create_cbr(folder: str, output_folder: str = "scans/cbr", name: str = ""):
+def create_cbr(input_folder: str, name: str = "", output_folder: str = "scans/cbr"):
     """Create a CBR archive from all the scans contained in the specified folder."""
-    if not os.path.isdir(folder):
+    input_folder = input_folder[:-1] if input_folder[-1] == '/' else input_folder
+    
+    if not os.path.isdir(input_folder) or len(os.listdir(input_folder)) == 0:
         return
         
-    if not name:
-        name = folder.split('/')[-1]
+    if not name or len(name) == 0:
+        name = input_folder.split('/')[-1]
         
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     
     cbr_path = f"{output_folder}/{name}.cbr"
     with ZipFile(cbr_path, mode='w') as cbr:
-        for scan in os.listdir(folder):
-            cbr.write(f"{folder}/{scan}")
+        for scan in os.listdir(input_folder):
+            cbr.write(f"{input_folder}/{scan}")
             
     return cbr_path
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog="scans_dl", description="Download and archive anime scans.")
+    subparsers = parser.add_subparsers(dest="command")
+    
+    dl_parser = subparsers.add_parser("download", aliases=["dl", "d"], help="Download all anime scans at the given address.")
+    dl_parser.add_argument("address", help="The address of the webpage(s) containing the scans.")
+    dl_parser.add_argument("--range", "-r", type=str, help="The range of the chapters to download, in the form a-b. Use '{}' in the address to specify where to increment the chapter. You can also specify a single number, which will be the last chapter to download.")
+    dl_parser.add_argument("--name", "-n", type=str, default="", help="The name of the manga.")
+    dl_parser.add_argument("--foldername", "-f", type=str, default="", help="The name of the folder containing the scans to be downloaded.")
+    dl_parser.add_argument("--output", "-o", type=str, help="The path where the scan(s) should be downloaded. A new folder will be created.")
+    
+    cbr_parser = subparsers.add_parser("cbr", help="Archive all the scans present in the given folder(s) in CBR format.")
+    cbr_parser.add_argument("path", type=str, help="The path of the folder(s) containing the scans to archive.")
+    cbr_parser.add_argument("--all", action="store_true", help="Look for all the subfolders in the given folder and create all the corresponding archives.")
+    cbr_parser.add_argument("--name", "-n", type=str, default="", help="The name of the manga.")
+    cbr_parser.add_argument("--filename", "-f", type=str, default="", help="The name of the archive(s) to create.")
+    cbr_parser.add_argument("--output", "-o", type=str, help="The path where the archive(s) should be created.")
+    
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    
+    args = parser.parse_args()
+    
+    if args.command in ["download", "dl", "d"]:
+        if "{}" in args.address and not args.range:
+            print("Error: '{}' was found in the address, but no range was specified.", file=sys.stderr)
+            sys.exit(1)
+        
+        if args.range:
+            if "{}" not in args.address:
+                print("Error: a range was specified, but '{}' missing in the address.", file=sys.stderr)
+                sys.exit(1)
+            
+            raw_range = args.range.split('-')
+            dl_range = None
+            
+            if len(raw_range) > 2 or '' in raw_range:
+                print("Error: please specify the range in the form 'a-b', where a < b, i.e '1-10'. Or, specify a single number that will act as the last number of the range.", file=sys.stderr)
+                sys.exit(1)
+            
+            raw_range = [int(x) for x in raw_range]
+                
+            if len(raw_range) == 1:
+                dl_range = range(1, raw_range[0] + 1)
+            else:
+                dl_range = range(raw_range[0], raw_range[1] + 1)
+                
+            for i in dl_range:
+                download(
+                    args.address.format(i),
+                    args.foldername + '-' + str(i) if args.foldername else "",
+                    args.output if args.output else "scans/raw" + ('/' + args.name if args.name else "")
+                )
+        
+        else:
+            download(
+                args.address,
+                args.foldername,
+                args.output if args.output else "scans/raw" + ('/' + args.name if args.name else "")
+            )
+        
+    elif args.command == "cbr":
+        if args.all:
+            i = 0
+            for chapter in os.listdir(args.path):
+                if os.path.isfile(chapter):
+                    continue
+                
+                i += 1
+                path = create_cbr(
+                    f"{args.path}/{chapter}",
+                    args.filename + '-' + str(i) if args.filename else "",
+                    args.output if args.output else "scans/cbr/" + args.path.split('/')[-1]
+                )
+                
+                print(f"Created CBR file for {chapter}: {path}")
+                
+            print(f"\nCreated {i} CBR files.")
+            
+        else:
+            path = create_cbr(
+                args.path,
+                args.filename,
+                args.output if args.output else "scans/cbr/" + args.path.split('/')[-1]
+            )
+            print(f"\nArchive created: {path}")
